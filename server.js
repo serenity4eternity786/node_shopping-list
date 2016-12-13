986f8528babb5ec84d4a31e082254dfd1b4468df
Alisha Byford <serenity4eternity786@gmail.com>
2016-12-13 02:00:47 +0000

    updating the delete function

--- server.js
+++ server.js
@@ -2,11 +2,38 @@ var express = require('express');
 
 var Storage = {
   add: function(name) {
-    var item = {name: name, id: this.setId};
+    var item = {
+      name: name,
+      id: this.setId
+    };
     this.items.push(item);
     this.setId += 1;
     return item;
-  } 
+  },
+  delete: function(id) {
+
+    if (!this.items[id]) {
+      //Id doesn't exist, return an error
+    }
+    else {
+      //delete this.items[id];
+      return this.items.splice(id, 1);
+    }
+    return this.items;
+  },
+  update: function(id, name) {
+
+    var msg;
+    if (this.items[id]) {
+      //item already exists, update it
+      this.items[id].name = name;
+    }
+    else {
+      //no such item # exists, create it
+      storage.add(name);
+    }
+    return this.items;
+  }
 };
 
 var createStorage = function() {
@@ -26,29 +53,32 @@ var app = express();
 app.use(express.static('public'));
 
 app.get('/items', function(request, response) {
-    response.json(storage.items);
+  response.json(storage.items);
 });
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 app.post('/items', jsonParser, function(request, response) {
-    if (!('name' in request.body)) {
-        return response.sendStatus(400);
-    }
+  if (!('name' in request.body)) {
+    return response.sendStatus(400);
+  }
 
-    var item = storage.add(request.body.name);
-    response.status(201).json(item);
+  var item = storage.add(request.body.name);
+  response.status(201).json(item);
 });
 
-app.delete('/items/:id', function(request, response) {
-    if (item) {
-    return response.sendStatus(201);
-  } else {
+app.delete('/items/:id', jsonParser, function(request, response) {
+  if (!request.body) {
     return response.sendStatus(400);
-}
+  }
+
+  var item = storage.delete(request.params.id);
+
+  response.status(200).json(item);
+
 });
 
 app.put('items/:id', function(request, response) {
-    if (!req.body) {
+  if (!req.body) {
     return response.sendStatus(400);
   }
   var item = storage.put(req.params.id, req.body.name);
